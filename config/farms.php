c15fb8e0a8400e8f237868fc8063d7a8c04be732
Seb35 <seb35@seb35.fr>
2016-03-19 10:00:33 +0100

    Update config files
    Already used in my dev environment (adapted)

--- config/farms.php
+++ config/farms.php
@@ -8,18 +8,21 @@ return array(
 		'variables' => array(
 		
 			array( 'variable' => 'family',
-			       'file'     => '$family.dblist',
 			),
 			array( 'variable' => 'lang',
+			       'file'     => 'org/$family.dblist',
 			       'type'     => 'language',
 			),
 		),
+		'suffix' => '$family',
+		'wikiID' => '$lang$family',
 		'data' => '/srv/data/org/$family/$lang',
 		'cache' => '/tmp/mw-cache/org-$version-$family-$lang',
 		'config' => array(
 			'org/InitialiseSettings.php',
 			'org/PrivateSettings.php',
 		),
+		'exec-config' => 'org/ExecSettings.php',
 	),
 	
 	# Configuration for a small wiki farm
@@ -28,19 +31,22 @@ return array(
 		'variables' => array(
 			
 			array( 'variable' => 'client',
-			       'file'     => 'clients.yml',
+			       'file'     => 'com/example/clients.yml',
 			       'config'   => 'com/example/$client/InitialiseSettings.yml',
 			),
 			array( 'variable' => 'wiki',
-			       'file'     => '$client/wikis.yml',
+			       'file'     => 'com/example/$client/wikis.yml',
 			),
 		),
+		'suffix' => '$family',
+		'wikiID' => '$wiki-$client',
 		'data' => '/srv/data/com/example/$client/$wiki',
 		'cache' => '/tmp/mw-cache/com-example-$version-$client-$wiki',
 		'config' => 'com/example/InitialiseSettings.yml',
 		'post-config' => array(
 			'com/example/PrivateSettings.yml',
-			'com/example/GlobalSettings.php',
+			'com/example/GlobalSettings.yml',
 		),
+		'exec-config' => 'com/example/ExecSettings.php',
 	),
 );
