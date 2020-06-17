f952df7cf51a6885dd152a4a572038b00e73ac1e
Seb35 <seb35@seb35.fr>
2020-06-17 18:15:52 +0200

    Add endpoint 'rest.php'.
    Change-Id: Ib2a4c8a947a4455bf2d6ad227509c840a9b1b5d8

---
+++ www/rest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Entry point rest.php in the context of a multiversion MediaWiki farm.
+ *
+ * @package MediaWikiFarm
+ * @author Sébastien Beyou ~ Seb35 <seb35@seb35.fr>
+ * @license GPL-3.0-or-later
+ * @license AGPL-3.0-or-later
+ */
+// @codeCoverageIgnoreStart
+
+# Default MediaWikiFarm configuration
+$wgMediaWikiFarmCodeDir = dirname( dirname( dirname( __FILE__ ) ) );
+$wgMediaWikiFarmConfigDir = '/etc/mediawiki';
+$wgMediaWikiFarmCacheDir = '/tmp/mw-cache';
+$wgMediaWikiFarmSyslog = 'mediawikifarm';
+
+# Check the entry point is installed in a multiversion MediaWiki farm or in the classical MediaWiki extensions directory
+if( is_file( dirname( $wgMediaWikiFarmCodeDir ) . '/includes/DefaultSettings.php' ) ) {
+	exit;
+}
+
+# Override default MediaWikiFarm configuration
+@include_once dirname( dirname( __FILE__ ) ) . '/config/MediaWikiFarmDirectories.php';
+
+# Include library
+require_once dirname( dirname( __FILE__ ) ) . '/src/MediaWikiFarm.php';
+
+# Redirect to the requested version
+if( MediaWikiFarm::load( 'rest.php' ) == 200 ) {
+	require 'rest.php';
+}
+// @codeCoverageIgnoreEnd
